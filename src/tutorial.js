804564ba1a852179c2f007847a185df0398cdd55
Ruaridh MacDowall <ruaridhmacdowall@gmail.com>
2021-02-21 11:47:53 +0000

    tutorial

--- src/tutorial.js
+++ src/tutorial.js
@@ -18,7 +18,15 @@ export default () => (
         <div className="content">
           {' '}
           Click and drag to place cells. <br/>
-	  Or press 'Random' to start with a random grid.
+	  Or press 'Random' to start with a random grid. <br/> 
+	  Press 'Run' to start the simulation. <br/>
+	    <b> Explanation </b> <br/>
+	At each step in time, the following transitions occur: <br/>
+
+Any live cell with fewer than two live neighbours dies, as if by underpopulation. <br/>
+Any live cell with two or three live neighbours lives on to the next generation. <br/>
+Any live cell with more than three live neighbours dies, as if by overpopulation. <br/>
+Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
         </div>
         <div className="actions">
           <button
